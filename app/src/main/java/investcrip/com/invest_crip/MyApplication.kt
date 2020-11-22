8097cce54a23fe542dc2bb17adc11facf6994fcb
Joao Oliveira <joaopaulo_391@hotmail.com>
2020-11-22 01:44:58 -0300

    Bump de versão do retrofit e inicializa modulo Koin na application

--- app/src/main/java/investcrip/com/invest_crip/MyApplication.kt
+++ app/src/main/java/investcrip/com/invest_crip/MyApplication.kt
@@ -1,6 +1,7 @@
 package investcrip.com.invest_crip
 
 import android.app.Application
+import investcrip.com.invest_crip.features.crypto.di.cryptoModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -11,11 +12,7 @@ class MyApplication : Application() {
 
         startKoin {
             androidContext(this@MyApplication)
-            modules(
-                    listOf(
-
-                    )
-            )
+            modules(listOf(cryptoModule))
         }
     }
 }
